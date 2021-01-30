2721c1d56e817db8cc52901c86747b895fe8d226
rlagkdud <ha0936861@gmail.com>
2021-01-30 17:23:13 +0900

    titles and subtitles

--- Weather.js
+++ Weather.js
@@ -7,44 +7,64 @@ import { LinearGradient } from 'expo-linear-gradient';
 const weatherOptions = {
     Thunderstorm:{
         iconName:"weather-lightning-rainy",
-        gradient:["#003973","#E5E5BE"]
+        gradient:["#003973","#E5E5BE"],
+        title:"Thunderstorm",
+        subtitle:"Stay home"
     },
     Drizzle:{
         iconName:"weather-rainy",
-        gradient:["#E55D87","#5FC3E4"]
+        gradient:["#E55D87","#5FC3E4"],
+        title:"Drizzle",
+        subtitle:"Bring an unbrella!"
     },
     Rain:{
         iconName:"weather-rainy",
-        gradient:["#292E49","#536976"]
+        gradient:["#292E49","#536976"],
+        title:"Rain",
+        subtitle:"Bring an unbrella!"
     },
     Snow:{
         iconName:"weather-snowy",
-        gradient:["#403B4A","#E7E9BB"]
+        gradient:["#403B4A","#E7E9BB"],
+        title:"Snow",
+        subtitle:"Watch your steps!"
     },
     Clear:{
         iconName:"weather-sunny",
-        gradient:["#1A2980","#26D0CE"]
+        gradient:["#1A2980","#26D0CE"],
+        title:"Sunny",
+        subtitle:"Let's go out and play!"
     },
     Clouds:{
         iconName:"weather-cloudy",
-        gradient:["#FF8008","#FFC837"]
+        gradient:["#FF8008","#FFC837"],
+        title:"Clouds",
+        subtitle:"Cloudy cloudy cloudy"
     },
     
     Haze:{
         iconName:"weather-hazy",
-        gadient:["#334d50","#cbcaa5"]
+        gadient:["#334d50","#cbcaa5"],
+        title:"Haze",
+        subtitle:"Hazy hazy hazy"
     },
     Fog:{
         iconName:"weather-fog",
-        gadient:["#757F9A","#D7DDE8"]
+        gadient:["#757F9A","#D7DDE8"],
+        title:"Fog",
+        subtitle:"Foggy foggy foggy"
     },
     Squall:{
         iconName:"weather-pouring",
-        gadient:["#00416A","#799F0C","#FFE000"]
+        gadient:["#00416A","#799F0C","#FFE000"],
+        title:"Squall",
+        subtitle:"Stay home"
     },
     Tornado:{
         iconName:"weather-tordano",
-        gadient:["#2c3e50","#bdc3c7"]
+        gadient:["#2c3e50","#bdc3c7"],
+        title:"Tornado",
+        subtitle:"Don't go outside"
     },
 }
 export default function Weather({ temp,condition }) {
@@ -57,7 +77,10 @@ export default function Weather({ temp,condition }) {
                 <MaterialCommunityIcons name={weatherOptions[condition].iconName } size={96} color="white" />
                 <Text style={styles.temp}>{temp}°</Text>
             </View>
-            <View style={styles.halfContainer}/>
+            <View style={{...styles.halfContainer, ...styles.textContent}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+            </View>
         </LinearGradient>
     )
 };
@@ -92,5 +115,19 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems:"center",
         justifyContent:"center"
+    },
+    title:{
+        color:"white",
+        fontWeight:"300",
+        fontSize:44,
+        marginBottom:10
+    },
+    subtitle:{
+        color:"white",
+        fontWeight:"600",
+        fontSize:24,
+    },
+    textContent:{
+        alignItems:"flex-start"
     }
 })
